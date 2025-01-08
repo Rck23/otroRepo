dc4585e474cfa9aa017e2ecea0b401fc98e966d3
Curso Git <curso.git@gmail.com>
2025-01-08 14:40:45 -0600

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="otroRepo",
+    version="0.1.1", 
+    packages=find_packages(),
+    description="Un paquete pip simple de saludo",
+    author="Ulises",
+    author_email="U@gmail.com",
+    url="https://github.com/Rck23/otroRepo"
+)
